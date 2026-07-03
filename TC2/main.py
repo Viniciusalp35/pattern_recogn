ab2e76765e6e59f8bdbea567baaab0b02cd2d534
Viniciusalp35 <vinicius.alp5626@gmail.com>
2026-07-02 21:26:35 -0300

    feat: questao 03 e 04 de inversa finalizada

--- TC2/main.py
+++ TC2/main.py
@@ -14,7 +14,6 @@ def comparator(data:np.ndarray):
     # MATRIZ REFERENCIA
     C_ref = ref_matrix(data)[0]
     cov_matrix = [cov(data)[0] for cov in covs]
-    print(cov_matrix)
     norm_result = []
     for i in range(len(cov_matrix)):
         print(f"DIFERENÇA ENTRE COV_{i+1} E REFERENCIA:")
@@ -26,7 +25,6 @@ def comparator(data:np.ndarray):
 
 def time_comparator(data:np.ndarray):
     m,n = data.shape
-    print(data.shape)
     time_array = []
     for i,cov in enumerate(covs):
         for _ in range(100):
@@ -49,6 +47,49 @@ def time_comparator(data:np.ndarray):
     sns.violinplot(df_slow,y='cov_function',x='time_(ms)',ax=axs[1,0])
     sns.histplot(df_slow,hue="cov_function",x='time_(ms)',ax=axs[1,1])
     plt.savefig(f"./results/time_comparison_{n}_readings.png",dpi=300)
+
+
+def _wellnes_check(c_mat,n):
+    rank = np.linalg.matrix_rank(c_mat)
+    cond = np.linalg.cond(c_mat)
+    rcond= 1/cond if cond != 0 else 0.0
+    wellness = rank == n and rcond > 1e-6
+    return rank,rcond,wellness
+
+def questao03(data:np.ndarray,loader_obj:Loader):
+    m,n = data.shape
+    fastest_method = covs[-1]
+    C_global = fastest_method(data)[0]
+    rank_g,rcond_g,wellness_g = _wellnes_check(C_global,n)
+    c_by_class = []
+    score_by_class = [("GLOBAL",rank_g,rcond_g,wellness_g)]
+    data_by_class , classes = loader_obj.cov_by_class()
+    
+    for df,class_name in zip(data_by_class,classes):
+        cov = fastest_method(df)[0]
+        c_by_class.append((cov,class_name))
+        rank,rcond,wellness = _wellnes_check(cov,n)
+        score_by_class.append((class_name,rank,rcond,wellness))
+    df = pd.DataFrame(data=score_by_class, columns=["Class",'POSTO','RCOND','INVERTIBILIDADE'])
+    df.to_csv(f"./results/questao03/invertibilidade_{n}_readings.csv")
+    return c_by_class
+
+def questao04(cov_list: list[(np.ndarray,str)]):
+    result = []
+    for cov,class_name in cov_list:
+        try:
+            inv_c = np.linalg.inv(cov)
+            with open(f"./results/questao04/{cov.shape[0]}_records_inversas.txt",'a+') as f:
+                f.write(f"--------- {class_name} INVERSE ---------\n")
+                f.write(f"{np.array2string(inv_c)}\n\n")
+            result.append((f"{class_name}",'true'))
+        except Exception as e:
+            print("NAO DEU PARA INVERTER: ",e)
+            result.append((f"{class_name}",'false'))
+    
+    df = pd.DataFrame(data=result, columns=['CLASSE DA INVERSA','INVERTEU?'])
+    df.to_csv("./results/questao04/relation_class_invertible.csv")
+
     
 
 
@@ -59,10 +100,20 @@ if __name__ == "__main__":
     data_4 = loader_4.df
     loader_24 = Loader("./data/sensor_readings_24.data",[f"US{i}"for i in range(24)]+["Class"])
     data_24 = loader_24.df
+    print("\n\n\n---------- Questão 01 ----------")
     #QUESTAO 01
     ROW = [];COLUMNS = ['cov usado','norma da diferença']
     ROW += comparator(data_4); ROW += comparator(data_24)
     df = pd.DataFrame(data=ROW, columns=COLUMNS, index=['4_readings' if i < len(ROW)/2 else '24_readings' for i in range(len(ROW))]); df.to_csv("results/Questao01.csv")
+    print("\n\n\n---------- Questão 02 ----------")
     #QUESTAO 02
     time_comparator(data_4)
     time_comparator(data_24)
+    print("\n\n\n---------- Questão 03 ----------")
+    #QUESTAO 03
+    c_3_4 = questao03(data_4,loader_4)
+    c_3_24 = questao03(data_24,loader_24)
+    print("\n\n\n---------- Questão 04 ----------")
+    #QUESTAO 04:
+    questao04(c_3_4)
+    questao04(c_3_24)
--- TC2/modules/Loader.py
+++ TC2/modules/Loader.py
@@ -7,14 +7,15 @@ class Loader():
     def __init__(self, path:str = DATA_PATH, names= COL_NAMES):
         self.df_raw = pd.read_csv(path,header=None, names=names)
         self.df = np.asarray(self.df_raw.iloc[:,:-1])
+        self.data_by_class = None
 
-    def cov_by_class(self):
+    def cov_by_class(self) -> list[np.ndarray]:
         data_array = []
         classes = self.df_raw['Class'].unique()
         for c in classes:
-            data_array.append(self.df_raw[self.df_raw['Class']==c])
-        print(data_array)
-        return data_array
+            self.data_by_class = self.df_raw[self.df_raw['Class']==c]
+            data_array.append(np.asarray(self.data_by_class.iloc[:,:-1]))
+        return data_array,classes
 
 
 if __name__ == "__main__":
